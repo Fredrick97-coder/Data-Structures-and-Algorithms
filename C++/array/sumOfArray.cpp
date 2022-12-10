f0655b688c10f53b08b238731c386a1e35c8e272
Software Engineer (FullStack) <fredrickotabil97@gmail.com>
2022-12-10 12:54:58 +0000

    [C++] - datatype modifiers

--- C++/array/sumOfArray.cpp
+++ C++/array/sumOfArray.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <float.h>
 using namespace std;
 
 int main()
 {
  int marks[5] = {2, 4, 1, 5, 6};
+ float avg_marks = 0;
  int sum = 0;
  int arr_len = sizeof(marks) / sizeof(int);
+ int h_marks = 0;
+
  for (int i = 0; i < arr_len; i++)
  {
   sum += marks[i];
+  if (marks[i] > h_marks)
+  {
+   h_marks = marks[i];
+  }
  }
 
- cout << "sum of the array is: ";
- cout << sum << endl;
- ;
+ avg_marks = sum / float(arr_len);
+ cout << "sum of the array is: " << sum << endl;
+ cout << "average of the marks is: " << avg_marks << endl;
+ cout << "Size of sum stored in memory: " << sizeof(sum) << "byte(s)" << endl;
+ cout << "The highest mark is: " << h_marks << endl;
 
  return 0;
 }
